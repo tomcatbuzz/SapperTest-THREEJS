370eb16f684bfd3d5367aa9949b072359c857bb1
Tony <tomcatbuzz@yahoo.com>
2020-12-21 11:23:23 -0500

    initial

---
+++ src/routes/about/index.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { customtransition } from "../../components/transition"
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<div class="content" in:customtransition out:fade>
+	<h1>About this site</h1>
+	
+	<p>This is the 'about' page. There's not much here.</p>
+</div>
---
+++ src/routes/contacts/index.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { customtransition } from "../../components/transition"
+</script>
+
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<div class="content" in:customtransition out:fade>
+<h1>Contact this site</h1>
+
+<p>This is the 'contact' page. There's not much here.</p>
+</div>
